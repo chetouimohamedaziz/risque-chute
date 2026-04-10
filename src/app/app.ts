11c68f1737c8a00747a886f49c33aedb8649ece5
Mohamed-Aziz CHETOUI <chetoui.mohamedaziz@outlook.fr>
2026-04-10 23:32:29 +0200

    Impl header & footer & description & confidentialité

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+import { APP_WORDING } from './constants/app-wording.constant';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [MatToolbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('risque-chute');
+  protected readonly appWording = APP_WORDING;
 }
---
+++ src/app/constants/app-wording.constant.ts
@@ -0,0 +1,10 @@
+import packageJson from '../../../package.json';
+
+export const APP_WORDING = {
+  appTitle: 'VigilÂge',
+  appVersion: `v${packageJson.version}`,
+  selogan: 'Votre partenaire de sécurité pour une vie autonome et sereine',
+  description: ' est une application dédiée à la sécurité de nos aînés. Elle a été spécialement conçue pour aider les seniors à prévenir les chutes chez leurs proches âgés en identifiant les risques souvent invisibles du quotidien.',
+  privacyTitle: '🔒 Confidentialité',
+  privacyText: 'Vos réponses resteront anonymes et confidentielles. Ce questionnaire vise à mieux comprendre vos connaissances, pratiques et besoins en matière de prévention des chutes.',
+};
