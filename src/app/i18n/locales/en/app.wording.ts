76df77289d188b5a94ec4d5ad73e8e29b909c87c
Mohamed-Aziz CHETOUI <chetoui.mohamedaziz@outlook.fr>
2026-04-16 22:04:34 +0200

    Mise à jour des fichiers wording

--- src/app/i18n/locales/en/app.wording.ts
+++ src/app/i18n/locales/en/app.wording.ts
@@ -2,9 +2,9 @@ import { AppWording } from '../../types';
 
 export const enAppWording: Omit<AppWording, 'appVersion'> = {
   appTitle: 'VigilAge',
-  selogan: 'Your safety partner for an autonomous and peaceful life',
+  selogan: 'Your safety partner for an independent and peaceful life',
   description:
-    ' is an application dedicated to the safety of seniors. It has been specially designed to help prevent falls among older loved ones by identifying often invisible everyday risks.',
+    ' is an application dedicated to the safety of seniors. It has been specially designed to help prevent falls among older loved ones by identifying often invisible everyday risks. It contains 12 yes/no questions. Each "yes" answer scores 1 point, while each "no" answer scores 0 points. A high score indicates a greater risk of falling.',
   privacyTitle: '🔒 Privacy',
   privacyText:
     'Your answers remain anonymous and confidential. This questionnaire aims to better understand your knowledge, practices, and needs regarding fall prevention.',
--- src/app/i18n/locales/fr/app.wording.ts
+++ src/app/i18n/locales/fr/app.wording.ts
@@ -4,7 +4,7 @@ export const frAppWording: Omit<AppWording, 'appVersion'> = {
   appTitle: 'VigilAge',
   selogan: 'Votre partenaire de sécurité pour une vie autonome et sereine',
   description:
-    ' est une application dédiée à la sécurité de nos aînés. Elle a été spécialement conçue pour aider les seniors à prévenir les chutes chez leurs proches âgés en identifiant les risques souvent invisibles du quotidien.',
+    ' est une application dédiée à la sécurité de nos aînés. Elle a été spécialement conçue pour aider les seniors à prévenir les chutes chez leurs proches âgés en identifiant les risques souvent invisibles du quotidien. Elle comporte 12 questions à réponses dichotomiques (oui/non). Chaque réponse « oui » rapporte 1 point, tandis que chaque réponse « non » vaut 0 point. Un score élevé indique un risque de chute plus important.',
   privacyTitle: '🔒 Confidentialité',
   privacyText:
     'Vos réponses resteront anonymes et confidentielles. Ce questionnaire vise à mieux comprendre vos connaissances, pratiques et besoins en matière de prévention des chutes.',
